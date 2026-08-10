f63b069e945d361e39941f15e59b72c0a7665013
kalebe <kalebebarros991@gmail.com>
2026-08-10 11:08:28 -0300

    Add files via upload

---
+++ exercico pratico/hello.js
@@ -0,0 +1,4 @@
+console.log("Olá, mundo! Este é meu primeiro script Node.js!");
+console.log("Parabéns! Você está Programando!");
+console.log ("Node.js versão:" , process.version);
+console.log ("Sistema operacional:" , process.platform);
---
+++ exercico pratico/minha-padaria-app/app.js
@@ -0,0 +1,4 @@
+const colors =  require('colors');
+console.log('Bem Vindo à Padaria do João!'.rainbow);
+console.log('Pães frecos todos os dias'.green);
+console.log('Horário: 6h às 18h'.blue);
